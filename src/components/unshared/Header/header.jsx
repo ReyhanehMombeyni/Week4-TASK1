d49ead6bb2e11e7b1a3e5618b51aa57c8140ad3f
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-02-13 18:16:10 +0330

    add responsive Header

--- src/components/unshared/Header/header.jsx
+++ src/components/unshared/Header/header.jsx
@@ -12,7 +12,20 @@ const Header = () => {
                 <h1 className={Styles.title}>Where Young Minds Blossom and <span>Dreams Take Flight.</span></h1>
                 <p className={Styles.desc}>Our kinder garden school provides a nurturing and stimulating environment, fostering a love for learning that lasts a lifetime. Join us as we embark on an exciting educational journey together!</p>
                 <div className={Styles.statistics}>
-                    
+                <div className={Styles.rate}>
+                    <div className={Styles.rateItem}>
+                        <span>+7000</span>
+                        <p>Students Passed Out</p>
+                    </div>
+                    <div className={Styles.rateItem}>
+                        <span>+37</span>
+                        <p>Awards & Recognitions</p>
+                    </div>
+                    <div className={Styles.rateItem}>
+                        <span>+15</span>
+                        <p>Experience Educators</p>
+                    </div>
+                </div>
                 </div>
             </div>
         </header>
