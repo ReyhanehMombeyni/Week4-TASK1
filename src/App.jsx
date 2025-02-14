6b84c3dc5b47cb51ff4b9488c155c21819633345
Reyhaneh <reyhaneh.sa.mo@gmail.com>
2025-02-14 17:50:33 +0330

    add icon Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Benefits from './components/unshared/Benefits/benefits.jsx';
+import Footer from './components/unshared/Footer/footer.jsx';
 import Header from './components/unshared/Header/header.jsx';
 import Nav from "./components/unshared/Nav/nav.jsx"
 
@@ -12,7 +13,7 @@ function App() {
       <Benefits />
       <Benefits />
       <Benefits />
-      
+      <Footer />
     </div>
   )
 }
---
+++ src/components/shared/Icon/icon.jsx
@@ -0,0 +1,11 @@
+import Styles from './icon.module.css';
+import { FaChessQueen } from "react-icons/fa";
+
+const Icon =() => {
+    return (
+        <div className={Styles.container}>
+                <FaChessQueen />
+        </div>
+    )
+}
+export default Icon;
